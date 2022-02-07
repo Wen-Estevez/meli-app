fea5f496cbd96383193f5c01bdd1dc78f6750796
unknown <westevez04@gmail.com>
2022-02-07 00:01:13 -0500

    prueba estilos barra de busqueda

--- src/views/SearchBar.jsx
+++ src/views/SearchBar.jsx
@@ -5,11 +5,20 @@ import { Link } from 'react-router-dom';
 import logo from "../assets/Logo_ML@2x.png.png.png";
 
 const BarContainer = styled.aside`
-
+    background: #ffe600;
 `;
 
 const TextBar = styled.input`
-
+    margin-top:1rem;
+    margin-left: 10%;
+    width: 60%;
+    height:35px;
+    border: none;
+    border-radius: 3px;
+    -webkit-box-shadow: 0px 1px 2px -1px #242424bf;
+    -moz-box-shadow:  0px 1px 2px -1px #242424bf;
+    box-shadow:  0px 1px 2px -1px #242424bf;
+    position: absolute;
 `;
 
 const SearchButton = styled.button`
@@ -32,7 +41,7 @@ export default function SearchBar() {
             <LogoImage src={logo} ></LogoImage>
             <TextBar onChange={(e) => setInput(e.target.value)}></TextBar>
             <MenuL to={`/items?search=${input}`}>
-                <SearchButton >Enviar</SearchButton>
+                <SearchButton> Enviar</SearchButton>
             </MenuL>
         </BarContainer>
     )
