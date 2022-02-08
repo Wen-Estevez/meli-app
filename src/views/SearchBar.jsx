84a8db51315077b50168ecd97f25b7a575dda8a7
unknown <westevez04@gmail.com>
2022-02-07 23:15:50 -0500

    estilos barra de busqueda

--- src/views/SearchBar.jsx
+++ src/views/SearchBar.jsx
@@ -3,30 +3,46 @@ import { useState } from "react";
 import { Link } from 'react-router-dom';
 
 import logo from "../assets/Logo_ML@2x.png.png.png";
+import search from "../assets/ic_Search.png";
 
 const BarContainer = styled.aside`
     background: #ffe600;
 `;
 
 const TextBar = styled.input`
-    margin-top:1rem;
-    margin-left: 10%;
+    margin:0.8rem;
+    margin-left: 1%;
     width: 60%;
-    height:35px;
+    height:30px;
     border: none;
     border-radius: 3px;
     -webkit-box-shadow: 0px 1px 2px -1px #242424bf;
     -moz-box-shadow:  0px 1px 2px -1px #242424bf;
     box-shadow:  0px 1px 2px -1px #242424bf;
     position: absolute;
+    &:focus {
+        outline: none;
+    }
 `;
 
 const SearchButton = styled.button`
-
+    background: #f0efef url(${search}) no-repeat center;
+    height:32px;
+    width: 32px;
+    border:none;
+    position: absolute;
+    margin-left: 61%;
+    margin-top: 0.8em;
+    border-radius: 3px;
+    -webkit-box-shadow: 0px 1px 2px -1px #242424bf;
+    -moz-box-shadow:  0px 1px 2px -1px #242424bf;
+    box-shadow:  0px 1px 2px -1px #242424bf;
 `;
 
 const LogoImage = styled.img`
-
+    height: 30px;
+    margin-left: 12%;
+    padding:0.8rem;
 `;
 
 const MenuL = styled(Link)`                           //Estilo link
@@ -39,9 +55,9 @@ export default function SearchBar() {
     return (
         <BarContainer>
             <LogoImage src={logo} ></LogoImage>
-            <TextBar onChange={(e) => setInput(e.target.value)}></TextBar>
+            <TextBar placeholder=" Nunca dejes de buscar" onChange={(e) => setInput(e.target.value)}></TextBar>
             <MenuL to={`/items?search=${input}`}>
-                <SearchButton> Enviar</SearchButton>
+                <SearchButton></SearchButton>
             </MenuL>
         </BarContainer>
     )
