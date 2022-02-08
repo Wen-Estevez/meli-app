eda82296ea8c9809464e38fa1652007ab8cb1d30
Wen-Estevez <westevez04@gmail.com>
2022-02-07 23:20:25 -0500

    Add files via upload

---
+++ src/views/ResultItem.jsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+import freeShip from "../assets/ic_shipping@2x.png.png.png";
+
+const ItemCard = styled.section`
+
+`;
+const Title = styled.h4`
+
+`;
+const Price = styled.span`
+
+`;
+const Image = styled.img`
+
+`;
+const Freeship = styled.img`
+
+`;
+const Condition = styled.span`
+
+`;
+
+export default function ResultItem(item) {
+
+    return (
+        <ItemCard key={item.id}>
+            <Image src={ item.picture}/>
+            <Title>{item.title}</Title>
+            {item.free_shipping?<Freeship src={freeShip} />:<></>}
+            <Price>{ item.price.amount}</Price>
+            <Condition>{ item.condition}</Condition>
+        </ItemCard>
+    )
+}
