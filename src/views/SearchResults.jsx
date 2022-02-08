05f02884a9d35b8f3e1c9f4a252e9aa717798d1e
Wen-Estevez <westevez04@gmail.com>
2022-02-07 23:19:46 -0500

    Results cards

--- src/views/SearchResults.jsx
+++ src/views/SearchResults.jsx
@@ -1,22 +1,33 @@
 import { useLocation } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 
 import { searchQuery } from "../services/endpoints";
+import ResultItem from "./ResultItem";
+
+const Results = styled.article`
+
+`;
+const Categories = styled.span`
+
+`;
 
 export default function SearchResults() {
+    let i = 0;
     const { params } = useLocation();
     const search = new URLSearchParams(params).getAll("search")[0];
     const [results, setResults] = useState([]);
-    
+    console.log(params)
     async function getResults() {
         setResults(await searchQuery(search))
     };
     
-    useEffect(() =>getResults(), []);
+    useEffect(() =>getResults(),[]);
     
     return(
-        <>
-            {results.author?results.author.name:<></>}
-        </>
+        <Results>
+            {results.categories?.map((category) => <Categories key={i++}>{category}</Categories>)}
+            {results.items?.map((item)=>ResultItem(item))}
+        </Results>
     )
 }
