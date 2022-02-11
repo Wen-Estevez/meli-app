ea0267c2536d58a827175ab3db851b27a9d68443
Wen-Estevez <westevez04@gmail.com>
2022-02-11 13:11:56 -0500

    Estilos terminados

--- src/views/ItemDetail.jsx
+++ src/views/ItemDetail.jsx
@@ -12,19 +12,20 @@ const Container = styled.div`
 
 const Section = styled.section`
     float:right;
+    width: 30%;
 `;
 
 const Card = styled.div`
     background: white;
     margin: 0 auto;
-    width:80%;
+    width:57%;
     padding:5%;
 
 `;
 
 const Image = styled.img`
-    height:25rem;
-    width:45rem;
+    height:auto;
+    width:70%;
 `;
 
 const Title = styled.h2`
@@ -36,7 +37,9 @@ const Price = styled.span`
 `;
 
 const Decimals = styled.span`
-
+    margin-top: 2.5px;
+    margin-left: 2px;
+    position:absolute;
 `;
 
 const Info = styled.span`
@@ -50,15 +53,23 @@ const BuyButton = styled.button`
     color:white;
     padding:0.5rem 4rem;
     margin:0 auto;
+    margin-top: 10%;
+    width: 100%;
 `;
 
 const DescTitle = styled.h4`
-    font-size:2rem;
+    font-size: 25px;
+    color: #555555;
 `;
 
 const DescText = styled.span`
-
+    color: #555555;
 `;
+const DescContainer = styled.div`
+    width: 70%;
+    text-align: justify;
+`;
+
 
 export default function ItemDetail() {
     const id=useParams().id;                        //Guarda el id de la barra de direcciones
@@ -78,12 +89,14 @@ export default function ItemDetail() {
                 <Section>
                     <Info>{item?.item.condition}</Info>
                     <Title>{item?.item.title}</Title>
-                    <Price>${item?.item.price.amount}</Price>
+                    <Price>$ {item?.item.price.amount}</Price>
                     <Decimals>{item?.item.price.decimals}</Decimals>
                     <BuyButton>Comprar</BuyButton>
                 </Section>
                 <DescTitle>Descripción del producto</DescTitle>
-                <DescText>{item?.item.description}</DescText>
+                <DescContainer>
+                    <DescText>{item?.item.description}</DescText>
+                </DescContainer>
             </Card>
         </Container>
     )
